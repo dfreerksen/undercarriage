5a75927d4b643313149bfe93c2c7a94638ab8e9e
David Freerksen <dfreerksen@gmail.com>
2021-03-05 07:45:38 -0600

    Undercarriage::Controllers::KaminariConcern

---
+++ lib/undercarriage/controllers/kaminari_concern.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module Undercarriage
+  module Controllers
+    ##
+    # Kaminari pagination
+    #
+    # Helpers for Kaminari style pagination.
+    #
+    # Example
+    #   class ExamplesController < ApplicationController
+    #     include Undercarriage::Controllers::KaminariConcern
+    #
+    #     def index
+    #       @examples = Examples.page(page_num).per(per_page)
+    #     end
+    #   end
+    #
+    module KaminariConcern
+      extend ActiveSupport::Concern
+
+      included do
+        helper_method :page_num, :per_page
+      end
+
+      ##
+      # Items per page
+      #
+      # The number of items to return in pagination. Will use the Kaminari config `default_per_page` (typically `25`)
+      # for the count and will look for `per` in the URL paramaters to override.
+      #
+      # This is asseccible from the View as `per_page`
+      #
+      # Usage
+      #   /examples?per=100 # Return 100 items per page
+      #   /examples?per=10&page=3 # Return page 3 of items with 10 items per page
+      #
+      # @return [Integer] the number of items per page
+      #
+      def per_page
+        params.fetch(per_page_key, per_page_default).to_i
+      end
+
+      ##
+      # Page number
+      #
+      # Will look for the Kaminari config `param_name` (typically `page`) in the URL paramaters.
+      #
+      # This is asseccible from the View as `page_num`
+      #
+      # Usage
+      #   /examples?page=5 # Return page 5 of items
+      #   /examples?per=10&page=3 Return page 3 of items with 10 items per page
+      #
+      # @return [Integer] the page number
+      #
+      def page_num
+        params.fetch(page_num_key, page_num_default).to_i
+      end
+
+      protected
+
+      def per_page_key
+        :per
+      end
+
+      def page_num_key
+        Kaminari.config.param_name
+      end
+
+      private
+
+      def per_page_default
+        Kaminari.config.default_per_page
+      end
+
+      def page_num_default
+        1
+      end
+    end
+  end
+end
